fa8ef7ea943689334368e7dbe4ff16bd8a76c1d5
Hasan Niftiyev <hasan.niftiyev@yahoo.com>
2023-06-02 19:28:13 -0700

    start styling PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,17 +1,52 @@
 import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
+import { Box } from "@mui/material";
+
+const rootStyles = {
+  backgroundColor: "blue",
+  heigh: "100%",
+  display: "flex",
+  alignItems: "flex-start",
+  justifyContent: "center",
+};
+const containerStyles = {
+  width: "50%",
+  display: "flex",
+  alignItems: "flex-start",
+  flexDirection: "column",
+  flexWrap: "wrap",
+};
+const navStyles = {
+  display: "flex",
+  width: "100%",
+  justifyContent: "space-between",
+  color: "#fff",
+};
+const palettesStyles = {
+  boxSizing: "border-box",
+  width: "100%",
+  display: "grid",
+  gridTemplateColumns: "repeat(3,30%)",
+  gridGap: "5%e",
+};
 
 class PaletteList extends Component {
   render() {
     const { palettes } = this.props;
     return (
-      <div>
-        <h1>React Colors</h1>
-        {palettes.map((palette) => (
-          <MiniPalette {...palette} />
-        ))}
-      </div>
+      <Box sx={rootStyles}>
+        <Box sx={containerStyles}>
+          <nav sx={navStyles}>
+            <h1>React Colors</h1>
+          </nav>
+          <Box sx={palettesStyles}>
+            {palettes.map((palette) => (
+              <MiniPalette {...palette} />
+            ))}
+          </Box>
+        </Box>
+      </Box>
     );
   }
 }
