7f5133c863ed4293190f3db1af09ac3c1ace1a7e
Hasan Niftiyev <hasan.niftiyev@yahoo.com>
2023-06-07 16:32:27 -0700

    finish refactor ColorBox styles

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -7,6 +7,7 @@ import { Box, Typography, Button } from "@mui/material";
 
 // Styles
 const colorBox = (theme, props) => ({
+  background: props.background,
   width: "20%",
   height: props.showFullPallete ? "25%" : "50%",
   margin: "0 auto",
@@ -18,6 +19,73 @@ const colorBox = (theme, props) => ({
     opacity: "1",
   },
 });
+
+const boxContent = {
+  position: "absolute",
+  width: "100%",
+  left: "0px",
+  bottom: "0px",
+  padding: "10px",
+  color: "black",
+  letterSpacing: "1px",
+  textTransform: "uppercase",
+  fontSize: "12px",
+};
+
+const copyOverlay = {
+  opacity: "0",
+  zIndex: "0",
+  width: "100%",
+  height: "100%",
+  transition: "transform 0.6s ease-in-out",
+  transform: "scale(0.1)",
+};
+
+const showOverlay = {
+  opacity: "1",
+  transform: "scale(50)",
+  zIndex: "10",
+  position: "absolute",
+};
+
+const copyMsg = {
+  position: "fixed",
+  left: "0",
+  right: "0",
+  top: "0",
+  bottom: "0",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+  fontSize: "4rem",
+  transform: "scale(0.1)",
+  opacity: "0",
+  color: "white",
+  "& h1": {
+    fontWeight: "400",
+    textShadow: "1px 2px black",
+    background: "rgba(255, 255, 255, 0.2)",
+    width: "100%",
+    textAlign: "center",
+    marginBottom: "0",
+    padding: "1rem",
+    textTransform: "uppercase",
+  },
+  "& p": {
+    fontSize: "2rem",
+    fontWeight: "100",
+  },
+};
+
+const showMsg = {
+  opacity: "1",
+  transform: "scale(1)",
+  zIndex: "25",
+  transition: "all 0.4s ease-in-out",
+  transitionDelay: "0.3s",
+};
+
 const copyText = (theme, props) => ({
   color: chroma(props.background).luminance() >= 0.2 ? "black" : "white",
 });
@@ -70,12 +138,12 @@ const copyButton = (theme, props) => ({
 class ColorBox extends Component {
   constructor(props) {
     super(props);
-    this.state = { copied: false };
+    this.state = { copied: false, zIndex: 0 };
     this.handleCopyState = this.handleCopyState.bind(this);
   }
 
   handleCopyState() {
-    this.setState({ copied: true }, () =>
+    this.setState({ copied: true, zIndex: this.maxZIndex++ }, () =>
       setTimeout(() => this.setState({ copied: false }), 1500)
     );
   }
@@ -83,27 +151,28 @@ class ColorBox extends Component {
   render() {
     const { name, background, moreUrl, showFullPallete } = this.props;
     const { copied } = this.state;
+    console.log("Rendered with copied state:", copied);
     return (
       <CopyToClipboard text={background} onCopy={this.handleCopyState}>
-        <Box style={{ background }} sx={(theme) => colorBox(theme, this.props)}>
-          <div
+        <Box sx={(theme) => colorBox(theme, this.props)}>
+          <Box
             style={{ background }}
-            className={`copy-overlay ${copied && "show"}`}
+            sx={copied ? { ...copyOverlay, ...showOverlay } : copyOverlay}
           />
-          <Box className={`copy-msg ${copied && "show"}`}>
+          <Box sx={copied ? { ...copyMsg, ...showMsg } : copyMsg}>
             <h1>copied!</h1>
             <Typography sx={(theme) => copyText(theme, this.props)}>
               {background}
             </Typography>
           </Box>
-          <div className="copy-container">
-            <div className="box-content">
+          <Box>
+            <Box sx={boxContent}>
               <Typography sx={(theme) => colorName(theme, this.props)}>
                 {name}
               </Typography>
-            </div>
+            </Box>
             <Button sx={(theme) => copyButton(theme, this.props)}>Copy</Button>
-          </div>
+          </Box>
           {showFullPallete && (
             <Link to={moreUrl} onClick={(e) => e.stopPropagation()}>
               <Typography sx={(theme) => seeMore(theme, this.props)}>
