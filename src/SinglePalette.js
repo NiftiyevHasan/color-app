05cb80779174e68129f781fae2073b30310dfc2a
Hasan Niftiyev <hasan.niftiyev@yahoo.com>
2023-06-07 18:27:05 -0700

    refactor palette

--- src/SinglePalette.js
+++ src/SinglePalette.js
@@ -3,6 +3,52 @@ import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import PaletteFooter from "./PaletteFooter";
 import { Link } from "react-router-dom";
+import { Box } from "@mui/material";
+
+// Styles
+const palette = {
+  height: "100vh",
+  display: "flex",
+  flexDirection: "column",
+};
+
+const colors = {
+  height: "90%",
+};
+
+const goBack = {
+  width: "20%",
+  height: "50%",
+  margin: "0 auto",
+  display: "inline-block",
+  position: "relative",
+  cursor: "pointer",
+  marginBottom: "-3.5px",
+  opacity: "1",
+  backgroundColor: "black",
+  "& a": {
+    color: "white",
+    width: "100px",
+    height: "30px",
+    position: "absolute",
+    display: "flex",
+    top: "50%",
+    left: "50%",
+    marginLeft: "-50px",
+    marginTop: "-15px",
+    textAlign: "center",
+    outline: "none",
+    background: "rgba(255, 255, 255, 0.3)",
+    fontSize: "1rem",
+    lineHeight: "30px",
+    textTransform: "uppercase",
+    border: "none",
+    textDecoration: "none",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "2px",
+  },
+};
 
 class SinglePalette extends Component {
   constructor(props) {
@@ -22,7 +68,6 @@ class SinglePalette extends Component {
         allColors[level].filter((color) => color.id === colorsToFilterBy)
       );
     }
-    console.log(shades);
     return shades.slice(1);
   }
   changeFormat(format) {
@@ -37,23 +82,23 @@ class SinglePalette extends Component {
         background={shade[format]}
         name={shade.name}
         key={shade.hex}
-        showFullPalette={false}
+        showFullPallete={false}
       />
     ));
     return (
-      <div className="SingleColorPalette Palette">
+      <Box sx={palette}>
         <Navbar handleChange={this.changeFormat} showSlider={false} />
-        <div className="Palette-colors">
+        <Box sx={colors}>
           {allShades}
-          <div className="go-back ColorBox">
+          <Box sx={goBack}>
             <Link to={`/palette/${id}`} className="back-button">
               Go Back
             </Link>
-          </div>
-        </div>
+          </Box>
+        </Box>
 
         <PaletteFooter paletteName={paletteName} emoji={emoji} />
-      </div>
+      </Box>
     );
   }
 }
